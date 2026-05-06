de9d621c0bdbb783fb5fcffc6412b225ac269c87
kdkir <kekirwin@gmail.com>
2026-05-05 23:24:51 -0400

    Correcting https://github.com/mitre/microsoft-windows-11-stig-baseline/pull/9#discussion_r3164798367

--- controls/SV-253338.rb
+++ controls/SV-253338.rb
@@ -34,6 +34,6 @@ If the system is configured to send audit records directly to an audit server, t
 
   describe registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog\Security') do
     it { should have_property 'MaxSize' }
-    its('MaxSize') { should be >= 1_024_000 }
+    its('MaxSize') { should be >= input('max_security_event_log_size') }
   end
 end
